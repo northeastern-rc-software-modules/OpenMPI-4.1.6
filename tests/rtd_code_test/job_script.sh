450ae6e96a6d396a416c8a7d898fb30f0bf44b34
Arsalan Akhter <a.akhter@northeastern.edu>
2024-12-23 09:46:40 -0500

    Initial testing

---
+++ tests/rtd_code_test/job_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=hello_world
+#SBATCH --output=result.txt
+#SBATCH --ntasks=4
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=2000
+
+module load OpenMPI/4.1.6
+mpirun -n 4 ./hello_world
